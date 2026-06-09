8dfd9d225b36d1bf549b1071d29ed460d2f35f34
Hector Araujo <hectorcardoso879@gmail.com>
2026-06-09 15:26:04 -0300

    security: remove firebase-config.js do tracking e adicionar ao .gitignore

--- assets/js/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAetN1hjVd9LMSUex0c4KGzkzpdSP7AQGU",
-  authDomain: "seinfra-dbf5e.firebaseapp.com",
-  projectId: "seinfra-dbf5e",
-  storageBucket: "seinfra-dbf5e.firebasestorage.app",
-  messagingSenderId: "1033717206698",
-  appId: "1:1033717206698:web:accac3ca55338990ea0ad3",
-};
